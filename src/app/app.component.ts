72b4674a91b5c06a35f0aa328c9a89ddbf78ed56
Henrik Varga <v-hvarga@hotels.com>
2021-12-17 15:23:34 +0100

    steam api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+const apikey = "852C5FC1C2C03B2470608ECA0686A8BA";
+const steamUserId = "76561198364413370";
+const apiUrl = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=";
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Origin': '*'
+  })
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'steam-playtime-app';
+export class AppComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getOwnedGames().subscribe(data => {
+      console.log(data);
+    })
+  }
+
+  getOwnedGames(): Observable<any> {
+    return this.http.get(apiUrl + apikey + "&steamId=" + steamUserId, httpOptions)
+  }
+
 }
